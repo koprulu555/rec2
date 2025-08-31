0603db857448d04ed39ce43a5d3c40ddb032a3fa
koprulu555 <androdunyasitvv@gmail.com>
2025-08-31 04:30:32 +0300

    Create config.php

---
+++ scripts/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'api' => [
+        'mainUrl' => 'https://m.prectv55.lol',
+        'swKey' => '4F5A9C3D9A86FA54EACEDDD635185/64f9535b-bd2e-4483-b234-89060b1e631c',
+        'userAgent' => 'Dart/3.7 (dart:io)',
+        'referer' => 'https://twitter.com/'
+    ],
+    'output' => [
+        'userAgent' => 'googleusercontent',
+        'pageCount' => 4
+    ]
+];
+?>
